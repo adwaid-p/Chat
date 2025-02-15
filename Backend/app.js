53dc395f479d1fbb3d01e81d23e5227c033b736b
adwaid-p <adwaidpaloli007@gmail.com>
2025-02-15 11:50:19 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3001
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
